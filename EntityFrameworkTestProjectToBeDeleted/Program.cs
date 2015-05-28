c2736aec4afe6d26f04623da56bdf6edec30206b
Toby Holland <toby.holland@trademe.co.nz>
2015-05-28 15:03:17 +1200

    Remove bin and obj folders and added to ignore

--- EntityFrameworkTestProjectToBeDeleted/Program.cs
+++ EntityFrameworkTestProjectToBeDeleted/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using KickMe.Data.EntityFramework;
+using KickMe.Data.Models;
 
 namespace EntityFrameworkTestProjectToBeDeleted
 {
@@ -11,11 +13,23 @@ namespace EntityFrameworkTestProjectToBeDeleted
     {
         static void Main(string[] args)
         {
+
+           
             KickMeContext db = new KickMeContext();
             var projects = db.Projects.ToList();
             var users = db.Users.ToList();
             var commitments = db.Commitments.ToList();
             var comments = db.Comments.ToList();
+
+
+             //var users = ActiveDirectoryService.GetAllUsers();
+            var mappedUsers = new List<User>();
+
+            mappedUsers.ForEach(u => db.Users.Add(u));
+            db.SaveChanges();
+
+
+
             Console.ReadKey();
         }
     }
--- KickMe.Data/Models/User.cs
+++ KickMe.Data/Models/User.cs
@@ -13,6 +13,7 @@ namespace KickMe.Data.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string UserName { get; set; }
         public string Email { get; set; }
     }
 }
